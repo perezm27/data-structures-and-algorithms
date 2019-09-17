9e70bf5ea505c99136eeba1c6a59bb95094062f8
MELFI PEREZ <perezmelfi@outlook.com>
2019-09-16 21:49:06 -0700

    removed deadcode

--- code401challenges/src/main/java/HashtableExercises/RepeatedWord.java
+++ code401challenges/src/main/java/HashtableExercises/RepeatedWord.java
@@ -24,14 +24,11 @@ public class RepeatedWord {
                 numOfOccurrences++;
                 repeateWord = wordsArr[i];
                 duplicatesTbl.add(repeateWord, String.valueOf(numOfOccurrences));
+        //Step 5: return as soon as a word occurs more than once.
                 return repeateWord + " is a duplicate & it occurred at least " + duplicatesTbl.get(repeateWord) + " times";
             }
 
         }
-
-        //Step 4: Need to keep track of how many times a single word occurs - (can increment value each time a word occurs so when we look up that
-                  // key the returned value is the number of times the word occured in the sentence)
-        //Step 5: return as soon as a word occurs more than once.
         return "Sorry you entered en empty String";
     }
 
